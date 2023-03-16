db1a06dde2e1c8ba74a383b9512816c85a566bcb
klyukina9013 <128088507+klyukina9013@users.noreply.github.com>
2023-03-16 21:52:03 +0300

    Add files via upload

---
+++ untitled2/src/test/java/reqres/PostLoginTest.java
@@ -0,0 +1,31 @@
+package reqres;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+
+
+public class PostLoginTest {
+
+    @Test
+    @DisplayName("Вызов метода POST /loginUser. Авторизация пользователя")
+    public void successPostLoginTest() throws IOException, URISyntaxException, InterruptedException {
+        byte[] out = "{\"email\":\"eve.holt@reqres.in\",\"password\":\"cityslicka\"}".getBytes(StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://reqres.in/api/login"))
+                .POST(HttpRequest.BodyPublishers.ofByteArray(out))
+                .build();
+
+        HttpResponse response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
